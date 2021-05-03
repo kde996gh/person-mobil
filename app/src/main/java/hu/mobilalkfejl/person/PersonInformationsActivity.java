565453b3b8f80833b9da3f4e5a31a9e64e972446
Karácsonyi Deniel <h765660@stud.u-szeged.hu>
2021-05-03 15:59:13 +0200

    notification

--- app/src/main/java/hu/mobilalkfejl/person/PersonInformationsActivity.java
+++ app/src/main/java/hu/mobilalkfejl/person/PersonInformationsActivity.java
@@ -1,9 +1,14 @@
 package hu.mobilalkfejl.person;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.job.JobInfo;
+import android.app.job.JobScheduler;
+import android.content.ComponentName;
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -19,7 +24,13 @@ import com.google.firebase.firestore.QueryDocumentSnapshot;
 import com.google.firebase.firestore.QuerySnapshot;
 import com.google.gson.Gson;
 
+import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+import java.util.Calendar;
 import java.util.Collection;
+import java.util.Date;
+import java.util.Locale;
+import java.util.TimeZone;
 
 import hu.mobilalkfejl.person.model.Person;
 
@@ -38,6 +49,11 @@ public class PersonInformationsActivity extends AppCompatActivity {
     private FirebaseFirestore mFirestore;
     private CollectionReference mPersons;
 
+    private NotificationHelper mNotificationHelper;
+    private JobScheduler mJobScheduler;
+
+
+
     Person person;
     String personJsonObject;
 
@@ -48,9 +64,9 @@ public class PersonInformationsActivity extends AppCompatActivity {
 
         this.overridePendingTransition(R.anim.anim_slide_in_left, R.anim.anim_slide_out_left);
 
-
     }
 
+    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
     @Override
     protected void onStart() {
         super.onStart();
@@ -71,6 +87,10 @@ public class PersonInformationsActivity extends AppCompatActivity {
         Gson gson = new Gson();
 
         person = gson.fromJson(personJsonObject, Person.class);
+        checkBirthDay(this.person);
+        mJobScheduler = (JobScheduler) getSystemService(JOB_SCHEDULER_SERVICE);
+      //  setJobScheduler();
+
 
         setTextViewContent(person);
     }
@@ -107,6 +127,52 @@ public class PersonInformationsActivity extends AppCompatActivity {
         intent.putExtra("personObjectInJson", personJsonObject);
         startActivity(intent);
     }
+
+    public void checkBirthDay(Person person){
+
+        Date date = new Date(); // your date
+        Calendar cal = Calendar.getInstance(TimeZone.getTimeZone("Europe/Paris"));
+        cal.setTime(date);
+        int year = cal.get(Calendar.YEAR);
+        int month = cal.get(Calendar.MONTH);
+        int day = cal.get(Calendar.DAY_OF_MONTH);
+
+        String[] bday = person.getBirthDate().split("-");
+        Log.i(LOG_TAG, "LOGTAG:  " + cal + " " + "year: " + month + " , day: "+ day);
+
+        if(month == Integer.parseInt(bday[1]) && day == Integer.parseInt(bday[2])){
+            mNotificationHelper = new NotificationHelper(this);
+
+             Log.i(LOG_TAG, "Bejöttem az ifbe!");
+            mNotificationHelper.send("Ma van a születésnapja: " + person.getName());
+
+
+        }
+
+
+
+    }
+
+//    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
+//    private void setJobScheduler() {
+//        // SeekBar, Switch, RadioButton
+//        int networkType = JobInfo.NETWORK_TYPE_UNMETERED;
+//        Boolean isDeviceCharging = true;
+//        int hardDeadline = 5000; // 5 * 1000 ms = 5 sec.
+//
+//        ComponentName serviceName = new ComponentName(getPackageName(), NotificationJobService.class.getName());
+//        JobInfo.Builder builder = new JobInfo.Builder(0, serviceName)
+//                .setRequiredNetworkType(networkType)
+//                .setRequiresCharging(isDeviceCharging)
+//                .setOverrideDeadline(hardDeadline);
+//
+//        JobInfo jobInfo = builder.build();
+//        mJobScheduler.schedule(jobInfo);
+//
+//        // mJobScheduler.cancel(0);
+//        // mJobScheduler.cancelAll();
+//
+//    }
 
     public void deletePerson(View view) {
 
